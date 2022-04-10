1f2362f1565e0b782f91efc348132762189719ab
@rzapata <rzapata@empresa.thedataage.com>
2022-04-10 16:59:01 -0600

    Initial commit

---
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+fun main() {
+    println("Happy Birthday!")
+    println("Jhansi")
+    println("You are 25!")
+}
